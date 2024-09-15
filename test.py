9103e886bfab095790aae6a5eb4b22d979680156
xort-sol <tomuhammadbilal485066@gmail.com>
2024-09-15 17:43:36 +0500

    Added the Workflows

--- test.py
+++ test.py
@@ -10,3 +10,26 @@ def test_home(client):
     """Test the home page"""
     response = client.get('/')
     assert response.status_code == 200
+    assert response.data == b'Hello World'
+
+def test_predict(client):
+    # Define the payload to send in the POST request
+    payload = {
+        'avg_income': 50000,
+        'house_age': 20,
+        'num_rooms': 3,
+        'num_bedrooms': 2,
+        'population': 1000
+    }
+
+    # Send a POST request to the /predict endpoint
+    response = client.post('/predict', 
+        data=json.dumps(payload), 
+        content_type='application/json')
+
+    # Assert the response status code
+    assert response.status_code == 200
+
+    # Assert the response content
+    response_json = json.loads(response.data)
+    assert 'prediction' in response_json
