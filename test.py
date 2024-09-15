a0761ee577a650c6db340a014de55bd7308e47de
xort-sol <tomuhammadbilal485066@gmail.com>
2024-09-15 17:34:31 +0500

    Added the Workflows

---
+++ test.py
@@ -0,0 +1,13 @@
+import pytest
+from index import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    """Test the home page"""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
--- test/test.py
+++
@@ -1,8 +0,0 @@
-import json
-from api import index
-
-def test_index_route(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
-    expected = '<h2 class="text-center">flask example ci/cd</h2>'
-    assert expected in res.get_data(as_text=True)
